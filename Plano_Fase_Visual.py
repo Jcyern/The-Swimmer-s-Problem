6cfac80f8c7e38cf1dabe15de05c3cfd574954bc
Juan Carlos <juancarlosyernoespinosa@gmail.com>
2025-11-23 23:01:53 -0500

    analalizar el tipo de punto critico

--- Plano_Fase_Visual.py
+++ Plano_Fase_Visual.py
@@ -7,15 +7,16 @@ def mostrar_mi_grafico():
     st.markdown( """ ### Sea  el sistema lineal  de altura lateral y(t) y su velocidad v(t):""")
 
     st.latex(r"""
-            
-            \begin{cases}
-            \Large
-            \frac{dy}{dt} = v \\ \\
-            \Large
-            \Large
-            \frac{dv}{dt} = -3v - 4y
-            \end{cases}
-            """)
+        
+        \begin{cases}
+        \Large
+        \frac{dy}{dt} = v \\ \\
+        \Large
+        \Large
+        \frac{dv}{dt} = -3v - 4y
+        \end{cases}
+        """)
+        
         
     st.markdown(" ##### A simple vista se nota que es   un sistema autónomo")
     st.markdown(" ##### Def: Sistema Autónomo")
@@ -32,6 +33,37 @@ def mostrar_mi_grafico():
 
             """)
     st.markdown(""" ##### Sustituyendo v en -3v -4y = 0  obtenemos : y = 0 """)
-    st.markdown(" ##### Luego el punto (y,v) crítico es (0,0)")
+    st.markdown(" ##### Luego el punto (y,v) crítico es (0,0)  \n \n \n")
+    st.markdown("""### Analizar  el tipo y estabilidad del punto crítico (0,0):
+                """)
+    st.markdown("##### Es un sistema lineal , por ende lo podemos escribir en forma matricial:")
+    st.latex(r"\frac{d}{dt} \begin{bmatrix} y \\ v \end{bmatrix} = \begin{bmatrix} 0 & 1 \\ -4 & -3 \end{bmatrix} \begin{bmatrix} y \\ v \end{bmatrix}")
+    st.markdown(r"""
+    ##### Calculamos los valores propios de $A$ resolviendo $\det(A - \lambda I)=0$:
+    
+    $
+    \lambda^2 + 3\lambda + 4 = 0
+    $
+
+    ###### Las soluciones son:
+    $
+    \lambda = \frac{-3 \pm \sqrt{-7}}{2} = -\frac{3}{2} \pm i\frac{\sqrt{7}}{2}
+    $
+                """)
+
+    st.markdown(r"""
+    #### Valores propios:
+    $
+    \lambda = \frac{-3 \pm \sqrt{-7}}{2} = -\frac{3}{2} \pm i\frac{\sqrt{7}}{2}
+    $
+    """)
+
+    st.markdown("""
+    - **Parte real**: $\\Re(\\lambda) = -\\frac{3}{2}$ (negativa)
+    - **Parte imaginaria**: $\\Im(\\lambda) = \\pm \\frac{\\sqrt{7}}{2}$ (distinta de cero)
+    """)
+
+    
+    
     
 mostrar_mi_grafico()
