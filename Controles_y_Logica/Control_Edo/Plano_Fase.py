c56d6f7c74033c369d72707da2f4bde801c89bed
Juan Carlos <juancarlosyernoespinosa@gmail.com>
2025-11-23 23:54:03 -0500

    Agregando la logica para crear un plano de fase

---
+++ Controles_y_Logica/Control_Edo/Plano_Fase.py
@@ -0,0 +1,33 @@
+import numpy as np
+#Logica del Plano de Fase 
+
+#--- sistema de ecuaciones diferenciales ---
+def sistema(t, z):
+    y, v = z
+    dy = v
+    dv = -3*v - 4*y
+    return [dy, dv]
+
+
+def plano_de_fase(y_range=(-6, 6), v_range=(-6, 6), n_points=20, condiciones=None):
+    """
+    Dibuja el plano de fase del sistema dinámico.
+    
+    Parámetros:
+    - y_range: tupla (min, max) para el eje y
+    - v_range: tupla (min, max) para el eje v
+    - n_points: número de divisiones de la malla
+    - condiciones: lista de condiciones iniciales [[y0, v0], ...]
+    """
+    # Malla de puntos
+    x =np.linspace(y_range[0], y_range[1], n_points)
+    y =np.linspace(v_range[0], v_range[1], n_points)
+
+    
+    Y, V = np.meshgrid(x,y)
+    DY, DV = sistema(Y,V)
+    
+    # Normalización de los vectores
+    Norma_2= np.sqrt(DY**2 + DV**2)
+    
+    return [DY/Norma_2, DV/Norma_2]
